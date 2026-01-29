91f6f9281214ad8dc2d2de99fc0928e00d61197e
Kedar Mane <121948861+kedar3016@users.noreply.github.com>
2026-01-29 13:40:00 +0530

    Delete DigitalHealthCard.API directory

--- DigitalHealthCard.API/Models/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DigitalHealthCard.API.Models
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdateAt {  get; set; }
-    }
-}
